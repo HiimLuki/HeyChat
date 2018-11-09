a576f7bbbcd1ec5177d1b03fe39469f9249059a1
Lukas <lukas.schneller@haw-hamburg.de>
2018-11-09 19:05:59 +0100

    Verkleinerung Bild

--- app/src/main/java/heycompany/heychat/StartActivity.java
+++ app/src/main/java/heycompany/heychat/StartActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,6 +17,7 @@ public class StartActivity extends AppCompatActivity {
     //Register und Login Button
     private Button mRegBtn;
     private Button mLoginBtn;
+    private ImageView mIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,7 @@ public class StartActivity extends AppCompatActivity {
 
         mRegBtn = (Button) findViewById(R.id.start_reg_btn);
         mLoginBtn = (Button) findViewById(R.id.start_login_btn);
+        mIcon = (ImageView) findViewById(R.id.Icon);
 
         final ExplosionField explosionField = ExplosionField.attach2Window(this);
 
@@ -31,15 +34,28 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent reg_intent = new Intent(StartActivity.this,RegisterActivity.class);
-                startActivity(reg_intent);
+                explosionField.explode(mIcon);
+
+                new Timer().schedule(
+                        new TimerTask(){
+
+                            @Override
+                            public void run(){
+
+                                Intent reg_intent = new Intent(StartActivity.this,RegisterActivity.class);
+                                startActivity(reg_intent);
+                            }
+
+                        }, 900);
+
+
             }
         });
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                explosionField.explode(mLoginBtn);
+                explosionField.explode(mIcon);
 
                 new Timer().schedule(
                         new TimerTask(){
